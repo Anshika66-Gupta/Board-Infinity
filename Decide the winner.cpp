6460e3212ee010635f87a4274055c2639f4469c7
Anshika Gupta <63110987+Anshika66-Gupta@users.noreply.github.com>
2020-10-03 18:14:16 +0530

    Create Decide the winner.cpp

---
+++ Decide the winner.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+
+using namespace std;
+
+int main() {
+  int X, Y;
+  
+  scanf("%d%d", &X, &Y);
+  if (Y - X >= 0) {
+    printf("ANMOL");
+  } else {
+    printf("ASHISH");
+  }
+}
